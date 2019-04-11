61e66e47b83ca580c701c468f559da81fe5caf23
Christopher Rogan <christopherrogan@ChristophersMBP.attlocal.net>
2019-04-10 21:56:37 -0500

    adding SUSYNANO format

---
+++ src/SUSYNANOBase.cc
@@ -0,0 +1,6 @@
+#define SUSYNANOBase_cxx
+#include "SUSYNANOBase.hh"
+#include <TH2.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+
---
+++ src/SUSYNANORunsBase.cc
@@ -0,0 +1,6 @@
+#define SUSYNANORuneBase_cxx
+#include "SUSYNANORuneBase.hh"
+#include <TH2.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+

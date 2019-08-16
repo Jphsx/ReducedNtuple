7f70255a564014a2a6935b7cbef72e469c3e1f80
Christopher Rogan <christophersrogan@gmail.com>
2019-08-16 15:21:55 -0500

    updated NANO event count files

---
+++ scripts/DoEventCount_NANO.sh
@@ -0,0 +1,7 @@
+rm -f EventCount_NANO_*.root
+hadd EventCount_NANO_Autumn18_102X.root Autumn18_102X/root/*/*.root
+hadd EventCount_NANO_Autumn18_SMS_102X.root Autumn18_102X_SMS/root/*/*.root
+hadd EventCount_NANO_Fall17_94X.root Fall17_94X/root/*/*.root
+hadd EventCount_NANO_Fall17_SMS_94X.root Fall17_94X_SMS/root/*/*.root
+hadd EventCount_NANO_Summer16_94X.root Summer16_94X/root/*/*.root
+hadd EventCount_NANO_Summer16_SMS_94X.root Summer16_94X_SMS/root/*/*.root
